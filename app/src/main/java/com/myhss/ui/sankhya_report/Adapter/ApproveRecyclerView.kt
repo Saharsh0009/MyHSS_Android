4bc9c27677aba25a7a185d7f52af3cdd624b81e5
Nikunj-Iguru <nikunj@iguru-india.com>
2024-01-25 17:06:36 +0530

    1) work item 674

--- app/src/main/java/com/myhss/ui/sankhya_report/Adapter/ApproveRecyclerView.kt
+++ app/src/main/java/com/myhss/ui/sankhya_report/Adapter/ApproveRecyclerView.kt
@@ -9,13 +9,14 @@ import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.myhss.Utils.DebouncedClickListener
-import com.uk.myhss.ui.my_family.Model.Datum
+import com.myhss.ui.sankhya_report.Model.sankhya.ActiveMember
+
 import java.util.*
 
 
 internal class ApproveRecyclerView(
     context: Context,
-    val athelets_Beans: List<Datum>,
+    val athelets_Beans: List<ActiveMember>,
     val arrayListUser: ArrayList<String>,
     val arrayListUserId: ArrayList<String>,
     var selectAllItems: Boolean,
@@ -23,7 +24,7 @@ internal class ApproveRecyclerView(
 ) :
     RecyclerView.Adapter<ApproveRecyclerView.sViewHolder>() {
 
-    val mItems = mutableListOf<Datum>()
+    val mItems = mutableListOf<ActiveMember>()
     var selectAll = false
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): sViewHolder {
@@ -50,7 +51,7 @@ internal class ApproveRecyclerView(
         return position.toLong()
     }
 
-    fun updateList(list: List<Datum>) {
+    fun updateList(list: List<ActiveMember>) {
         mItems.addAll(list)
         notifyDataSetChanged()
     }
@@ -78,7 +79,7 @@ internal class ApproveRecyclerView(
 
         var dealerid: String? = null
 
-        fun bindItems(athelets_Beans: Datum, selectAllItems: Boolean) {
+        fun bindItems(athelets_Beans: ActiveMember, selectAllItems: Boolean) {
             val tvname = itemView.findViewById(com.uk.myhss.R.id.tvContent) as TextView
             val checkBoxparent = itemView.findViewById(com.uk.myhss.R.id.chbContent) as CheckBox
 
@@ -89,21 +90,21 @@ internal class ApproveRecyclerView(
                 arrayListUser.clear()
                 arrayListUserId.clear()
 
-                if (athelets_Beans.middleName == "") {
+                if (athelets_Beans.middle_name == "") {
                     arrayListUser.remove(
-                        athelets_Beans.firstName!!.capitalize(Locale.ROOT) + " " +
-                                athelets_Beans.lastName!!.capitalize(Locale.ROOT)
+                        athelets_Beans.first_name!!.capitalize(Locale.ROOT) + " " +
+                                athelets_Beans.last_name!!.capitalize(Locale.ROOT)
                     )
 
-                    arrayListUserId.remove(athelets_Beans.memberId)
+                    arrayListUserId.remove(athelets_Beans.member_id)
                 } else {
                     arrayListUser.remove(
-                        athelets_Beans.firstName!!.capitalize(Locale.ROOT) + " " +
-                                athelets_Beans.middleName!!.capitalize(Locale.ROOT) + " " +
-                                athelets_Beans.lastName!!.capitalize(Locale.ROOT)
+                        athelets_Beans.first_name!!.capitalize(Locale.ROOT) + " " +
+                                athelets_Beans.middle_name!!.capitalize(Locale.ROOT) + " " +
+                                athelets_Beans.last_name!!.capitalize(Locale.ROOT)
                     )
 
-                    arrayListUserId.remove(athelets_Beans.memberId)
+                    arrayListUserId.remove(athelets_Beans.member_id)
                 }
 
 //                mItems.add(ar)
@@ -117,40 +118,40 @@ internal class ApproveRecyclerView(
                 arrayListUser.clear()
                 arrayListUserId.clear()
 
-                if (athelets_Beans.middleName == "") {
+                if (athelets_Beans.middle_name == "") {
                     (!arrayListUser.contains(
-                        athelets_Beans.firstName!!.capitalize(Locale.ROOT) + " " +
-                                athelets_Beans.lastName!!.capitalize(Locale.ROOT)
+                        athelets_Beans.first_name!!.capitalize(Locale.ROOT) + " " +
+                                athelets_Beans.last_name!!.capitalize(Locale.ROOT)
                     ))
                     arrayListUser.add(
-                        athelets_Beans.firstName!!.capitalize(Locale.ROOT) + " " +
-                                athelets_Beans.lastName!!.capitalize(Locale.ROOT)
+                        athelets_Beans.first_name!!.capitalize(Locale.ROOT) + " " +
+                                athelets_Beans.last_name!!.capitalize(Locale.ROOT)
                     )
 
                     (!arrayListUserId.contains(
-                        athelets_Beans.memberId
+                        athelets_Beans.member_id
                     ))
                     arrayListUserId.add(
-                        athelets_Beans.memberId!!
+                        athelets_Beans.member_id!!
                     )
 
                 } else {
                     (!arrayListUser.contains(
-                        athelets_Beans.firstName!!.capitalize(Locale.ROOT) + " " +
-                                athelets_Beans.middleName!!.capitalize(Locale.ROOT) + " " +
-                                athelets_Beans.lastName!!.capitalize(Locale.ROOT)
+                        athelets_Beans.first_name!!.capitalize(Locale.ROOT) + " " +
+                                athelets_Beans.middle_name!!.capitalize(Locale.ROOT) + " " +
+                                athelets_Beans.last_name!!.capitalize(Locale.ROOT)
                     ))
                     arrayListUser.add(
-                        athelets_Beans.firstName!!.capitalize(Locale.ROOT) + " " +
-                                athelets_Beans.middleName!!.capitalize(Locale.ROOT) + " " +
-                                athelets_Beans.lastName!!.capitalize(Locale.ROOT)
+                        athelets_Beans.first_name!!.capitalize(Locale.ROOT) + " " +
+                                athelets_Beans.middle_name!!.capitalize(Locale.ROOT) + " " +
+                                athelets_Beans.last_name!!.capitalize(Locale.ROOT)
                     )
 
                     (!arrayListUserId.contains(
-                        athelets_Beans.memberId
+                        athelets_Beans.member_id
                     ))
                     arrayListUserId.add(
-                        athelets_Beans.memberId!!
+                        athelets_Beans.member_id!!
                     )
                 }
 
@@ -159,61 +160,61 @@ internal class ApproveRecyclerView(
 
             if (checkBoxparent.isChecked) {
 
-                if (athelets_Beans.middleName == "") {
+                if (athelets_Beans.middle_name == "") {
                     (!arrayListUser.contains(
-                        athelets_Beans.firstName!!.capitalize(Locale.ROOT) + " " +
-                                athelets_Beans.lastName!!.capitalize(Locale.ROOT)
+                        athelets_Beans.first_name!!.capitalize(Locale.ROOT) + " " +
+                                athelets_Beans.last_name!!.capitalize(Locale.ROOT)
                     ))
                     arrayListUser.add(
-                        athelets_Beans.firstName!!.capitalize(Locale.ROOT) + " " +
-                                athelets_Beans.lastName!!.capitalize(Locale.ROOT)
+                        athelets_Beans.first_name!!.capitalize(Locale.ROOT) + " " +
+                                athelets_Beans.last_name!!.capitalize(Locale.ROOT)
                     )
 
                     (!arrayListUserId.contains(
-                        athelets_Beans.memberId
+                        athelets_Beans.member_id
                     ))
                     arrayListUserId.add(
-                        athelets_Beans.memberId!!
+                        athelets_Beans.member_id!!
                     )
 
                 } else {
                     (!arrayListUser.contains(
-                        athelets_Beans.firstName!!.capitalize(Locale.ROOT) + " " +
-                                athelets_Beans.middleName!!.capitalize(Locale.ROOT) + " " +
-                                athelets_Beans.lastName!!.capitalize(Locale.ROOT)
+                        athelets_Beans.first_name!!.capitalize(Locale.ROOT) + " " +
+                                athelets_Beans.middle_name!!.capitalize(Locale.ROOT) + " " +
+                                athelets_Beans.last_name!!.capitalize(Locale.ROOT)
                     ))
                     arrayListUser.add(
-                        athelets_Beans.firstName!!.capitalize(Locale.ROOT) + " " +
-                                athelets_Beans.middleName!!.capitalize(Locale.ROOT) + " " +
-                                athelets_Beans.lastName!!.capitalize(Locale.ROOT)
+                        athelets_Beans.first_name!!.capitalize(Locale.ROOT) + " " +
+                                athelets_Beans.middle_name!!.capitalize(Locale.ROOT) + " " +
+                                athelets_Beans.last_name!!.capitalize(Locale.ROOT)
                     )
 
                     (!arrayListUserId.contains(
-                        athelets_Beans.memberId
+                        athelets_Beans.member_id
                     ))
                     arrayListUserId.add(
-                        athelets_Beans.memberId!!
+                        athelets_Beans.member_id!!
                     )
                 }
 
                 arrayData = arrayListUser.toString().replace("[", "").replace("]", "").trim()
             } else {
 
-                if (athelets_Beans.middleName == "") {
+                if (athelets_Beans.middle_name == "") {
                     arrayListUser.remove(
-                        athelets_Beans.firstName!!.capitalize(Locale.ROOT) + " " +
-                                athelets_Beans.lastName!!.capitalize(Locale.ROOT)
+                        athelets_Beans.first_name!!.capitalize(Locale.ROOT) + " " +
+                                athelets_Beans.last_name!!.capitalize(Locale.ROOT)
                     )
 
-                    arrayListUserId.remove(athelets_Beans.memberId)
+                    arrayListUserId.remove(athelets_Beans.member_id)
                 } else {
                     arrayListUser.remove(
-                        athelets_Beans.firstName!!.capitalize(Locale.ROOT) + " " +
-                                athelets_Beans.middleName!!.capitalize(Locale.ROOT) + " " +
-                                athelets_Beans.lastName!!.capitalize(Locale.ROOT)
+                        athelets_Beans.first_name!!.capitalize(Locale.ROOT) + " " +
+                                athelets_Beans.middle_name!!.capitalize(Locale.ROOT) + " " +
+                                athelets_Beans.last_name!!.capitalize(Locale.ROOT)
                     )
 
-                    arrayListUserId.remove(athelets_Beans.memberId)
+                    arrayListUserId.remove(athelets_Beans.member_id)
                 }
 
                 //                        arrayListUser.remove(athelets_Beans[position].getmText1())
@@ -221,13 +222,13 @@ internal class ApproveRecyclerView(
                     arrayListUser.toString().replace("[", "").replace("]", "").trim()
             }
 
-            if (athelets_Beans.middleName == "") {
-                tvname.text = athelets_Beans.firstName!!.capitalize(Locale.ROOT) + " " +
-                        athelets_Beans.lastName!!.capitalize(Locale.ROOT)
+            if (athelets_Beans.middle_name == "") {
+                tvname.text = athelets_Beans.first_name!!.capitalize(Locale.ROOT) + " " +
+                        athelets_Beans.last_name!!.capitalize(Locale.ROOT)
             } else {
-                tvname.text = athelets_Beans.firstName!!.capitalize(Locale.ROOT) + " " +
-                        athelets_Beans.middleName!!.capitalize(Locale.ROOT) + " " +
-                        athelets_Beans.lastName!!.capitalize(Locale.ROOT)
+                tvname.text = athelets_Beans.first_name!!.capitalize(Locale.ROOT) + " " +
+                        athelets_Beans.middle_name!!.capitalize(Locale.ROOT) + " " +
+                        athelets_Beans.last_name!!.capitalize(Locale.ROOT)
             }
 
             //===========click listner of check box===============//
@@ -236,61 +237,61 @@ internal class ApproveRecyclerView(
 //                for (i in 0 until athelets_Beans) {
                 if (isChecked) {
 
-                    if (athelets_Beans.middleName == "") {
+                    if (athelets_Beans.middle_name == "") {
                         (!arrayListUser.contains(
-                            athelets_Beans.firstName!!.capitalize(Locale.ROOT) + " " +
-                                    athelets_Beans.lastName!!.capitalize(Locale.ROOT)
+                            athelets_Beans.first_name!!.capitalize(Locale.ROOT) + " " +
+                                    athelets_Beans.last_name!!.capitalize(Locale.ROOT)
                         ))
                         arrayListUser.add(
-                            athelets_Beans.firstName!!.capitalize(Locale.ROOT) + " " +
-                                    athelets_Beans.lastName!!.capitalize(Locale.ROOT)
+                            athelets_Beans.first_name!!.capitalize(Locale.ROOT) + " " +
+                                    athelets_Beans.last_name!!.capitalize(Locale.ROOT)
                         )
 
                         (!arrayListUserId.contains(
-                            athelets_Beans.memberId
+                            athelets_Beans.member_id
                         ))
                         arrayListUserId.add(
-                            athelets_Beans.memberId!!
+                            athelets_Beans.member_id!!
                         )
 
                     } else {
                         (!arrayListUser.contains(
-                            athelets_Beans.firstName!!.capitalize(Locale.ROOT) + " " +
-                                    athelets_Beans.middleName!!.capitalize(Locale.ROOT) + " " +
-                                    athelets_Beans.lastName!!.capitalize(Locale.ROOT)
+                            athelets_Beans.first_name!!.capitalize(Locale.ROOT) + " " +
+                                    athelets_Beans.middle_name!!.capitalize(Locale.ROOT) + " " +
+                                    athelets_Beans.last_name!!.capitalize(Locale.ROOT)
                         ))
                         arrayListUser.add(
-                            athelets_Beans.firstName!!.capitalize(Locale.ROOT) + " " +
-                                    athelets_Beans.middleName!!.capitalize(Locale.ROOT) + " " +
-                                    athelets_Beans.lastName!!.capitalize(Locale.ROOT)
+                            athelets_Beans.first_name!!.capitalize(Locale.ROOT) + " " +
+                                    athelets_Beans.middle_name!!.capitalize(Locale.ROOT) + " " +
+                                    athelets_Beans.last_name!!.capitalize(Locale.ROOT)
                         )
 
                         (!arrayListUserId.contains(
-                            athelets_Beans.memberId
+                            athelets_Beans.member_id
                         ))
                         arrayListUserId.add(
-                            athelets_Beans.memberId!!
+                            athelets_Beans.member_id!!
                         )
                     }
 
                     arrayData = arrayListUser.toString().replace("[", "").replace("]", "").trim()
                 } else {
 
-                    if (athelets_Beans.middleName == "") {
+                    if (athelets_Beans.middle_name == "") {
                         arrayListUser.remove(
-                            athelets_Beans.firstName!!.capitalize(Locale.ROOT) + " " +
-                                    athelets_Beans.lastName!!.capitalize(Locale.ROOT)
+                            athelets_Beans.first_name!!.capitalize(Locale.ROOT) + " " +
+                                    athelets_Beans.last_name!!.capitalize(Locale.ROOT)
                         )
 
-                        arrayListUserId.remove(athelets_Beans.memberId)
+                        arrayListUserId.remove(athelets_Beans.member_id)
                     } else {
                         arrayListUser.remove(
-                            athelets_Beans.firstName!!.capitalize(Locale.ROOT) + " " +
-                                    athelets_Beans.middleName!!.capitalize(Locale.ROOT) + " " +
-                                    athelets_Beans.lastName!!.capitalize(Locale.ROOT)
+                            athelets_Beans.first_name!!.capitalize(Locale.ROOT) + " " +
+                                    athelets_Beans.middle_name!!.capitalize(Locale.ROOT) + " " +
+                                    athelets_Beans.last_name!!.capitalize(Locale.ROOT)
                         )
 
-                        arrayListUserId.remove(athelets_Beans.memberId)
+                        arrayListUserId.remove(athelets_Beans.member_id)
                     }
 
                     //                        arrayListUser.remove(athelets_Beans[position].getmText1())
